268b6e11cd190ac4d197d5c58f6aa9faf7592b1c
David Nidever <dnidever@montana.edu>
2025-01-02 13:00:41 -0700

    updates to ray.py

--- python/raytrace/ray.py
+++ python/raytrace/ray.py
@@ -1,12 +1,14 @@
 import os
 import numpy as np
+from . import surface
 
 # class for light rays
 
 class Ray(object):
 
     def __init__(self,position,normal):
-        self.position = position
-        self.normal = normal
-
+        self.position = surface.Point(position)
+        self.normal = surface.Normal(normal)
+        self.history = []
+        
     # Needs to keep track of the past points and normals
