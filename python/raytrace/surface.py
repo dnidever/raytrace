19bfe5ae60383d72543d8f995e01b7f42d9967c4
David Nidever <dnidever@montana.edu>
2025-01-03 02:14:01 -0700

    updates

--- python/raytrace/surface.py
+++ python/raytrace/surface.py
@@ -3,6 +3,7 @@
 import os
 import numpy as np
 import copy
+from . import utils
 
 class Point(object):
     """ Class for a point."""
@@ -231,6 +232,55 @@ class Vector(object):
     def phi(self):
         """ Return the azimuthal angle (angle from positive x-axis) in degrees."""
         return np.rad2deg(np.arctan2(self.data[1],self.data[0]))
+
+    @property
+    def rotation(self):
+        """ Return the rotation matrix."""
+
+        # Example (using ZYX convention):
+        # Given a normal vector: n = [nx, ny, nz]
+        # Calculate the rotation matrix:
+        # R = [ [cos(yaw)*cos(pitch), cos(yaw)*sin(pitch)*sin(roll) - sin(yaw)*cos(roll),
+        #        cos(yaw)*sin(pitch)*cos(roll) + sin(yaw)*sin(roll)],
+        #       [sin(yaw)*cos(pitch), sin(yaw)*sin(pitch)*sin(roll) + cos(yaw)*cos(roll),
+        #        sin(yaw)*sin(pitch)*cos(roll) - cos(yaw)*sin(roll)],
+        #       [-sin(pitch), cos(pitch)*sin(roll), cos(pitch)*cos(roll)] ]
+        # Extract Euler angles:
+        # pitch = arcsin(-R[2, 0])
+        # yaw = atan2(R[1, 0], R[0, 0])
+        # roll = atan2(R[2, 1], R[2, 2])
+        r = self.r
+        data = self.data/self.r
+        rot = np.zeros((3,3),float)
+
+        #rot[0,:] = [data[0]*data[2], data[1]*data[2], -1]
+        #rot[1,:] = [-data[1], data[0], 0.0]
+        #rot[2,:] = data
+
+        #rot[0,:] = [-data[1], data[0], 0.0]
+        #rot[1,:] = [data[0]*data[2], data[1]*data[2], -1]
+        #rot[2,:] = data
+        
+        rot[0,:] = [self.x*self.z/r, self.y*self.z/r, -r]
+        rot[1,:] = [-self.y/r, self.x/r, 0.0]
+        rot[2,:] = [self.x, self.y, self.z]
+
+        #rot[:,0] = [self.x*self.z/r, self.y*self.z/r, -r]
+        #rot[:,1] = [-self.y/r, self.x/r, 0.0]
+        #rot[:,2] = [self.x, self.y, self.z]
+        
+        #rot[0,:] = [self.y/r, -self.x/r, 0]
+        #rot[1,:] = [self.x*self.z/r, self.y*self.z/r, -r]
+        #rot[2,:] = [self.x, self.y, self.z]
+
+        #alpha = np.arccos(-Z2/np.sqrt(1-Z3**2))
+        alpha = np.arctan2(Z1,-Z2)
+        eta = np.arccos(Z3)
+        #gamma = np.arccos(Y3/np.sqrt(1-Z3**2))
+        gamma = np.arctan2(X3,Y3)
+        
+        return rot
+        
     
     def copy(self):
         return Vector(self.data.copy())
@@ -243,7 +293,6 @@ class NormalVector(Vector):
         if len(data)!=3:
             raise ValueError('data needs three elements')
         self.__data = np.array(data).astype(float)
-        #super(NormalVector,self).__init__(data)
         self.__data /= np.linalg.norm(self.__data)   # normalize
 
     @property
@@ -268,27 +317,66 @@ class Line(object):
             raise ValueError('slopes needs three elements')
         # Parametric form for 3-D line
         # (x,y,z) = (x0,y0,z0)+t(a,b,c)
-        self.__point = Point(point)
-        self.__slopes = np.array(slopes).astype(float)
+        self.point = np.array(point).astype(float)
+        self.slopes = np.array(slopes).astype(float)
 
+    @property
+    def point(self):
+        return self.__point
+
+    @point.setter
+    def point(self,value):
+        if len(value)!=3:
+            raise ValueError('value needs three elements')
+        pnt = np.array(value).astype(float)
+        self.__point = pnt
+
+    @property
+    def slopes(self):
+        return self.__slopes
+
+    @slopes.setter
+    def slopes(self,value):
+        if len(value)!=3:
+            raise ValueError('value needs three elements')
+        slp = np.array(value).astype(float)
+        self.__slopes = slp
+    
     def __call__(self,t):
         """ Return the position at parametric value t """
         pos = np.zeros(3,float)
-        pos[0] = self.__point.x + t*self.__slopes[0]
-        pos[1] = self.__point.y + t*self.__slopes[1]
-        pos[2] = self.__point.z + t*self.__slopes[2]
+        pos[0] = self.point[0] + t*self.slopes[0]
+        pos[1] = self.point[1] + t*self.slopes[1]
+        pos[2] = self.point[2] + t*self.slopes[2]
         return Point(pos)
-        
+
+    @classmethod
+    def frompoints(cls,p1,p2):
+        """ Use two points to define the line. """
+        pnt1 = np.atleast_1d(p1).astype(float)
+        pnt2 = np.atleast_1d(p2).astype(float)
+        point = pnt1
+        slopes = (pnt2-pnt1)
+        return Line(point,slopes)
+    
     @property
     def data(self):
-        return self.__normal
+        return self.point,self.slopes
 
     def __array__(self):
         return self.data
+
+    def __repr__(self):
+        s = 'Line([(x,y,z)=({:.3f},{:.3f},{:.3f})+t({:.3f},{:.3f},{:.3f}))'.format(*self.point,*self.slopes)
+        return s
     
     def copy(self):
         return Line(self.data.copy())
 
+
+# circle
+# ellipse
+# cylinder
     
 class Surface(object):
     """ Main surface base class. """
@@ -310,8 +398,12 @@ class Surface(object):
         pass
 
     def dointersect(self,ray):
-        """ Does the ray intersect the the surface """
-        pass
+        """ Does the line intersect the surface """
+        intpts = self.intersections(line)
+        if len(intpts)==0:
+            return False
+        else:
+            return True
 
     def intersection(self,ray):
         """ Return the first intersection point """
@@ -323,10 +415,23 @@ class Surface(object):
 
 class Plane(Surface):
 
-    def __init__(self,**kw):
-        super().__init__(**kw)
+    def __init__(self,normal,d):
+        self.normal = NormalVector(normal)
+        self.d = d
         self.position = None
 
+    @property
+    def data(self):
+        dt = np.zeros(4,float)
+        dt[:3] = self.normal.data
+        dt[3] = self.d
+        return d
+        
+    @property
+    def equation(self):
+        s = '{:.3f}*x+{:.3f}*y+{:.3f}*z + {:.3f} = 0'.format(*self.normal,self.d)
+        return s
+    
     def distance(self,obj):
         """ Distance from a point and the normal of the plane. """
         if hasattr(obj,'center'):
@@ -337,10 +442,6 @@ class Plane(Surface):
             pnt = Point(obj)
         # 
         return dist
-
-    def dointersect(self,ray):
-        """ Does the ray intersect the the surface """
-        pass
 
     def intersection(self,ray):
         """ Return the first intersection point """
@@ -362,10 +463,6 @@ class Sphere(Surface):
         else:
             pnt = Point(obj)
         return np.linalg.norm(self.center-pnt)
-    
-    def dointersect(self,ray):
-        """ Does the ray intersect the the surface """
-        pass
 
     def intersection(self,ray):
         """ Return the first intersection point """
@@ -393,10 +490,6 @@ class HalfSphere(Surface):
         else:
             pnt = Point(obj)
         return np.linalg.norm(self.center-pnt)
-    
-    def dointersect(self,ray):
-        """ Does the ray intersect the the surface """
-        pass
 
     def intersection(self,ray):
         """ Return the first intersection point """
@@ -405,8 +498,8 @@ class HalfSphere(Surface):
     
 class Parabola(Surface):
 
-    def __init__(self,a,**kw):
-        super().__init__(**kw)
+    def __init__(self,a,position,normal):
+        #super().__init__(**kw)
         # a is the leading coefficient of the parabola and determines the shape
         # negative a is convex
         # positive a is concave
@@ -416,8 +509,30 @@ class Parabola(Surface):
         self.position = Point(position)
         self.normal = NormalVector(normal)
 
+        # parabola equation in 3D
+        # z = a*(x**2 + y**2)
+
+    @property
+    def a(self):
+        return self.__a
+
+    @a.setter
+    def a(self,value):
+        self.__a = a
+
+    @property
+    def center(self):
+        return self.position.data
+
+    @property
+    def vertex(self):
+        return self.position.data
+    
     @property
     def convex(self):
+        # a is the leading coefficient of the parabola and determines the shape
+        # negative a : convex
+        # positive a : concave
         return self.a<0
         
     def distance(self,obj):
@@ -428,14 +543,16 @@ class Parabola(Surface):
         else:
             pnt = Point(obj)
         return np.linalg.norm(self.center-pnt)
-
-    def dointersect(self,ray):
-        """ Does the ray intersect the the surface """
-        pass
-
-    def intersection(self,ray):
-        """ Return the first intersection point """
-        pass
+        
+    def intersections(self,line):
+        """ Return the intersection points """
+        # rotate the line into the Parabola reference frame
+        # line in parametric form
+        # substitute x/y/z in parabola equation for the line parametric equations
+        # solve quadratic equation in t
+        rline = line.rotate(self.normal)
+        intpts = utils.intersect_line_parabola(line,self)
+        return intpts
 
     @property
     def focus(self):
@@ -447,6 +564,11 @@ class Parabola(Surface):
         """ Focal length """
         return 1/(4*np.abs(self.__a))
 
+    @property
+    def axis_of_symmetry(self):
+        """ Return the axis of symmetry """
+        return Line(self.center,self.normal.data)
+    
     @property
     def directrix(self):
         """ Return the directrix. """
--- python/raytrace/utils.py
+++ python/raytrace/utils.py
@@ -3,6 +3,44 @@
 import os
 import numpy as np
 
+
+# intersection function
+def intersect_line_plane(p0, p1, p_co, p_no, epsilon=1e-6):
+    """
+    p0, p1: Define the line.
+    p_co, p_no: define the plane:
+        p_co Is a point on the plane (plane coordinate).
+        p_no Is a normal vector defining the plane direction;
+             (does not need to be normalized).
+
+    Return a Vector or None (when the intersection can't be found).
+    """
+
+    u = p1-p0
+    dot = p_no*u
+    #u = sub_v3v3(p1, p0)
+    #dot = dot_v3v3(p_no, u)
+
+    if abs(dot) > epsilon:
+        # The factor of the point between p0 -> p1 (0 - 1)
+        # if 'fac' is between (0 - 1) the point intersects with the segment.
+        # Otherwise:
+        #  < 0.0: behind p0.
+        #  > 1.0: infront of p1.
+        w = p0-p_co
+        fac = -(p_no-w0)/dot
+        u *= fac
+        out = p0+u
+        return out
+        #w = sub_v3v3(p0, p_co)
+        #fac = -dot_v3v3(p_no, w) / dot
+        #u = mul_v3_fl(u, fac)
+        #return add_v3v3(p0, u)
+    
+    # The segment is parallel to plane.
+    return None
+
+
 # intersection function
 def isect_line_plane_v3(p0, p1, p_co, p_no, epsilon=1e-6):
     """
@@ -69,3 +107,170 @@ def mul_v3_fl(v0, f):
         v0[1] * f,
         v0[2] * f,
     )
+
+def intersect_line_plane(line,plane):
+    # line in parametric form
+    # substitute x/y/z in parabola equation for the line parametric equations
+    # solve quadratic equation in t
+
+    # line
+    #  x = x0+sx*t
+    #  y = y0+sb*t
+    #  z = z0+sz*t
+    # plane
+    #  a*x + b*y + c*z + d = 0
+
+    #  a(x0+sx*t) + b(y0+sy*t) + c(z0+sz*t) + d = 0
+    #  (a*x0+b*y0+c*z0+d) + (a*sx+b*sy+c*sz)*t = 0
+    #  t = -(a*x0+b*y0+c*z0+d) / (a*sx+b*sy+c*sz)
+
+    # three options
+    # 1) one intersection
+    # 2) no intersection (parallel and offset)
+    # 3) many intersections (contained in the plane)
+
+    x0,y0,z0 = line.point
+    sx,sy,sz = line.slopes
+    a,b,c,d = plane.data
+    numer = -(a*x0 + b*y0 + c*z0 + d)
+    denom = a*sz + b*sy + c*sz
+
+    # 1 intersection
+    if denom != 0.0:
+        t = numer/denom
+        out = line(t)
+    # no intersection
+    elif denom == 0.0 and numer != 0.0:
+        out = None
+    # in the plane
+    elif denom == 0.0 and numer == 0.0:
+        out = np.inf
+
+    # Example of a line in the plane
+    # https://math.libretexts.org/Bookshelves/Calculus/Supplemental_Modules_(Calculus)/Multivariable_Calculus/1%3A_Vectors_in_Space/Intersection_of_a_Line_and_a_Plane
+    # line
+    #  x = 1 + 2t
+    #  y = -2 + 3t
+    #  z = -1 + 4t
+    # plane
+    #  x + 2y - 2z = -1
+    # (1+2t)+2(−2+3t)−2(−1+4t)=-1
+    # (1-4+2+1) + (2+6-8)*t = 0
+    # (0) + (0)*t = 0
+
+    return out
+
+
+def intersect_line_parabola(line,parabola):
+    # rotate the line into the Parabola reference frame
+    # line in parametric form
+    # substitute x/y/z in parabola equation for the line parametric equations
+    # solve quadratic equation in t
+
+    # line
+    #  x = x0+sx*t
+    #  y = y0+sb*t
+    #  z = z0+sz*t
+    # parabola
+    #  z = a*(x**2+y**2)
+
+    #  z0+sz*t = d*(x0+sx*t)^2 + a*(y0+sy*t)^2
+    #  z0/a + sz/d*t = x0^2+2*x0*sx*t+sx^2*t^2 + y0^2+2*y0*sy*t+sy^2*t^2
+    #  0 = (sx^2+sy^2)*t^2 + (2*x0*sx+2*y0*sy-sz/a)*t + (x0^2+y0^2-z0/a)
+    #  quadratic equation in t
+    #  t = -B +/- sqrt(B^2-4AC)
+    #      --------------------
+    #            2A
+
+    # A = sx^2+sy^2
+    # B = 2*x0*sx+2*y0*sy-sz/a
+    # C = x0^2+y0^2-z0/a
+    
+    # Three options, based on the discriminant
+    # 1) B^2-4AC > 0 : 2 solutions
+    # 2) B^2-4AC = 0 : 1 solution
+    # 3) B^2-4AC < 0 : 0 solutions
+
+    a = parabola.a
+    A = line.slopes[0]**2 + line.slopes[1]**2
+    B = 2*line.point[0]*line.slopes[0] + 2*line.point[1]*line.slopes[1] - line.slopes[2]/a
+    C = line.point[0]**2 + line.point[1]**2 - line.point[2]/a
+    discriminant = B**2-4*A*C
+
+    # 2 solutions
+    if discriminant > 0:
+        t1 = (-B-np.sqrt(discriminant))/(2*A)
+        t2 = (-B+np.sqrt(discriminant))/(2*A)
+        t = [t1,t2]
+        if t2<t1:
+            t = t[t2,t1]
+    # 1 solution
+    elif discriminant == 0:
+        t = [-B/(2*A)]
+    # 0 solutions
+    elif discriminant < 0:
+        t = []
+
+    # Get the points
+    #   just plug t into the line
+    out = []
+    for i in range(len(t)):
+        pt = line(t[i])
+        out.append(pt)
+
+    return out
+
+
+def normal_to_rot_matrix(normal_vector, angle):
+    """ Convert normal vector to a rotation matrix using Rodrigues' formula"""
+    n = normal_vector / np.linalg.norm(normal_vector)
+
+    skew_n = np.array([ [0, -n[2], n[1]], [n[2], 0, -n[0]], [-n[1], n[0], 0] ])
+
+    rot_matrix = np.eye(3) + np.sin(angle) * skew_n + (1 - np.cos(angle)) * np.outer(n, n)
+
+    return rot_matrix
+
+# Example usage
+
+#normal_vec = np.array([1, 0, 0])  # Rotation around X-axis
+
+#rotation_angle = np.radians(90)  # 90 degrees
+
+#rotation_matrix = normal_to_rot_matrix(normal_vec, rotation_angle)
+
+#print(rotation_matrix) 
+
+
+def euler_rot_matrix(alpha,beta,gamma,degrees=False):
+    # Input the euler angles
+    # https://en.wikipedia.org/wiki/Euler_angles
+
+    # Z_alpha X_beta Z_gamma convention
+    if degrees:
+        alpharad = np.deg2rad(alpha)
+        ca,sa = np.cos(alpharad),np.sin(alpharad)
+        betarad = np.deg2rad(beta)
+        cb,sb = np.cos(betarad),np.sin(betarad)
+        gammarad = np.deg2rad(gamma)
+        cg,sg = np.cos(gammarad),np.sin(gammarad)
+    else:
+        ca,sa = np.cos(alpha),np.sin(alpha)
+        cb,sb = np.cos(beta),np.sin(beta)
+        cg,sg = np.cos(gamma),np.sin(gamma)
+    rot = np.zeros((3,3),float)
+    rot[0,:] = [ca*cg-cb*sa*sg, -ca*sg-cb*cg*sa, sa*sb]
+    rot[1,:] = [cg*sa+ca*cb*sg, ca*cb*cg-sa*sg, -ca*sb]
+    rot[2,:] = [sb*sg, cg*sb, cb]
+    return rot
+
+def euler_angles_from_rot(rot,degrees=False):
+    # Z_alpha X_beta Z_gamma convention
+    alpha = np.arctan2(rot[0,2],-rot[1,2])   # arctan(R13/-R23)
+    beta = np.arccos(rot[2,2])               # arccos(R33)
+    gamma = np.arctan2(rot[2,0],rot[2,1])    # arctan(R31/R32)
+    if degrees:
+        alpha = np.rad2deg(alpha)
+        beta = np.rad2deg(beta)
+        gamma = np.rad2deg(gamma)
+    return alpha,beta,gamma
